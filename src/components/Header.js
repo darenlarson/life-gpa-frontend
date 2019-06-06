5950d820c682a15acb8a82808472690136ec47aa
darenlarson <larson.daren@gmail.com>
2019-06-05 22:38:31 -0500

    hide HOME and MANAGE HABITS buttons when not logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import './css/Header.css';
 
 const Header = props => {
+  const loggedIn = localStorage.getItem('token') ? true : false;
   return (
     <header className="header-wrapper">
       <div className="header-ctn">
@@ -12,8 +13,8 @@ const Header = props => {
 
         <nav>
           <div>
-            <NavLink to="/home" className={`nav-btn hide ${props.history.location.pathname === "/home" && "selected"}`}>HOME</NavLink>
-            <NavLink to="/home/manage-habits" className={`nav-btn hide ${props.history.location.pathname === "/home/manage-habits" && "selected"}`}>MANAGE HABITS</NavLink>
+            {loggedIn === true && <NavLink to="/home" className={`nav-btn hide ${props.history.location.pathname === "/home" && "selected"}`}>HOME</NavLink>}
+            {loggedIn === true &&  <NavLink to="/home/manage-habits" className={`nav-btn hide ${props.history.location.pathname === "/home/manage-habits" && "selected"}`}>MANAGE HABITS</NavLink>}
             <button className="nav-btn" onClick={(event) => props.logoutUser(event)}>LOGOUT</button>
           </div>
         </nav>
