441be95271b0a20e5e7c917661afa7e7dd4f1b34
darenlarson <larson.daren@gmail.com>
2019-06-19 20:41:07 -0500

    Stopped render logout button not logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
-import './css/Header.css';
+import "./css/Header.css";
 
 const Header = props => {
   // If loggedIn is true, won't render the HOME and MANAGE HABITS buttons
-  const loggedIn = localStorage.getItem('token') ? true : false;
+  const loggedIn = localStorage.getItem("token") ? true : false;
 
   return (
     <header className="header-wrapper">
       <div className="header-ctn">
         <div className="logo-wrapper">
-          <NavLink to="/home" className="logo"><h2>LifeGPA</h2></NavLink>
+          <NavLink to="/home" className="logo">
+            <h2>LifeGPA</h2>
+          </NavLink>
         </div>
         <nav>
           <div>
-            {loggedIn === true && <NavLink to="/home" className={`nav-btn hide ${props.history.location.pathname === "/home" && "selected"}`}>HOME</NavLink>}
-            {loggedIn === true &&  <NavLink to="/home/manage-habits" className={`nav-btn hide ${props.history.location.pathname === "/home/manage-habits" && "selected"}`}>MANAGE HABITS</NavLink>}
-            <button className="nav-btn" onClick={(event) => props.logoutUser(event)}>LOGOUT</button>
+            {loggedIn === true && (
+              <>
+                <NavLink
+                  to="/home"
+                  className={`nav-btn hide ${props.history.location.pathname ===
+                    "/home" && "selected"}`}
+                >
+                  HOME
+                </NavLink>
+                <NavLink
+                  to="/home/manage-habits"
+                  className={`nav-btn hide ${props.history.location.pathname ===
+                    "/home/manage-habits" && "selected"}`}
+                >
+                  MANAGE HABITS
+                </NavLink>
+                <button
+                  className="nav-btn"
+                  onClick={event => props.logoutUser(event)}
+                >
+                  LOGOUT
+                </button>
+              </>
+            )}
           </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
+
+Header.propTypes = {
+  logoutUser: PropTypes.func.isRequired
+};
 
 export default Header;
